c6c886b749b04e53805cb50c46a3622957430adc
thisistheway369 <thisistheway369@icloud.com>
2020-08-29 03:38:06 -0700

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
+    get '/' do
+        erb :index
+    end
+   
+    get "/info" do
+        "Testing the info page"
+    end
 
+    get '/' do
+        "<h1>Buster's Best Bowling Alley</h1>"
+    end
+    
 end
